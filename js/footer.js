08e0fe24141d47bbcf4c9ffc6156c66135f3fcca
Wang Xiaoyi <104486745@qq.com>
2023-05-08 11:59:47 +0800

    23.5.8 12:00

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 Vue.component("footBar", {
-  template: `
+    template: `
     <div class="foot">
     <div class="foot-box" :class="{active: activeBtn === 1}" @click="toPage(1)">
       <div class="foot-view"><i class="el-icon-s-home"></i></div>
@@ -22,20 +22,19 @@ Vue.component("footBar", {
     </div>
   </div>
   `,
-  data() {
-    return {
+    data() {
+        return {}
+    },
+    props: ['activeBtn'],
+    methods: {
+        toPage(i) {
+            if (i === 0) {
+                location.href = "/blog-edit.html"
+            } else if (i === 4) {
+                location.href = "/info.html"
+            } else if (i === 1) {
+                location.href = "/"
+            }
+        }
     }
-  },
-  props: ['activeBtn'],
-  methods: {
-    toPage(i) {
-      if (i === 0) {
-        location.href = "/blog-edit.html"
-      } else if (i === 4) {
-        location.href = "/info.html"
-      } else if (i === 1){
-        location.href = "/"
-      }
-    }
-  }
 })
